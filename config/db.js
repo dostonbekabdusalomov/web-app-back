e34255a73f6fad505eafe3a27afced188c57160b
“dostonbekabdusalomov” <dostonbek7535@gmail.com>
2026-07-28 17:41:22 +0500

    all

--- config/db.js
+++ config/db.js
@@ -6,8 +6,9 @@ const connectDB = async () => {
     console.log('MongoDB ulandi')
   } catch (err) {
     console.error('MongoDB xato:', err)
-    process.exit(1) // 💥 serverni 
+    process.exit(1) // 💥 serverni
   }
 }
+console.log('MONGO_URI:', process.env.MONGO_URI)
 
 module.exports = connectDB
